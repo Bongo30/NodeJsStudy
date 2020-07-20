ac6fee4e4d65181adab11ccbc7acc2016b6d95ed
빵끼빵끼 <sbg@ppangkkippangkkiui-MacBook-Pro.local>
2020-07-20 22:04:30 +0200

    day3

--- index2.js
+++ index2.js
@@ -3,6 +3,6 @@ var  http = require('http');
 
 http.createServer(function (req, res) {
 console.log('왜 두번씩 consol에 찍힐까요 ? 같이 고민');
-res.write('Hello World!'); //write a response to the client
+res.write('Hello World?!'); //write a response to the client
 res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
